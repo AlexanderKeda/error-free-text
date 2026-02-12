b4e823685aad7efd6263dedbfbee051423bfd69d
Alexander Keda <alex.keda94@yandex.ru>
2026-02-12 22:38:38 +0300

    Add text splitter util

---
+++ error-free-text-app/src/main/java/org/keda/errorfreetext/util/TextSplitter.java
@@ -0,0 +1,53 @@
+package org.keda.errorfreetext.util;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class TextSplitter {
+
+    private final int maxChunkSize;
+
+    public TextSplitter(
+            @Value("${text.splitter.max-chunk-size:10000}") int maxChunkSize
+    ) {
+        this.maxChunkSize = maxChunkSize;
+    }
+
+    public List<String> split(String text) {
+        if (text == null) {
+            return List.of();
+        } else if (text.length() <= maxChunkSize) {
+            return List.of(text);
+        }
+        return splitIntoChunks(text);
+    }
+
+    private List<String> splitIntoChunks(String text) {
+        List<String> chunks = new ArrayList<>();
+        int start = 0;
+        while (start < text.length()) {
+            int end = Math.min(start + maxChunkSize, text.length());
+            String chunk = text.substring(start, end);
+            int lastWhiteSpace = lastWhitespaceIndex(chunk);
+            if (lastWhiteSpace != -1 && lastWhiteSpace != 0 && end != text.length()) {
+                chunk = chunk.substring(0, lastWhiteSpace);
+            }
+                chunks.add(chunk);
+                start += chunk.length();
+        }
+        return chunks;
+    }
+
+    private int lastWhitespaceIndex(String text) {
+        for (int i = text.length() - 1; i >= 0; i--) {
+            if (Character.isWhitespace(text.charAt(i))) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
---
+++ error-free-text-app/src/test/java/org/keda/errorfreetext/util/TextSplitterTest.java
@@ -0,0 +1,44 @@
+package org.keda.errorfreetext.util;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TextSplitterTest {
+
+    private TextSplitter splitter = new TextSplitter(10);
+
+    @ParameterizedTest
+    @ValueSource(strings = {"Hello", "abcdefghij", ""})
+    void shouldReturnSingleChunkWhenTextLengthLessOrEqualsThanMaxChunkSize(String text) {
+        List<String> chunks = splitter.split(text);
+        assertEquals(1, chunks.size());
+        assertEquals(text, chunks.get(0));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"abc def ghi jkl mno pqr", "abcdefghijklmno"})
+    void shouldSplitTextIntoChunksWhenTextLengthGreaterThanMaxChunkSize(String text) {
+        List<String> chunks = splitter.split(text);
+
+        for (String chunk : chunks) {
+            assertTrue(chunk.length() <= 10);
+        }
+
+        String reconstructed = String.join("", chunks);
+        assertEquals(text, reconstructed);
+    }
+
+    @Test
+    void shouldReturnEmptyListWhenTextIsNull() {
+        String text = null;
+        List<String> chunks = splitter.split(text);
+        assertTrue(chunks.isEmpty());
+    }
+
+}
