fd4c6148db1d9b1f212bac8a9f71d0a036976053
Alexander Keda <alex.keda94@yandex.ru>
2026-02-13 20:47:39 +0300

    add unit tests for task processing services

---
+++ error-free-text-app/src/test/java/org/keda/errorfreetext/core/services/CorrectionTaskProviderTest.java
@@ -0,0 +1,59 @@
+package org.keda.errorfreetext.core.services;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.keda.errorfreetext.core.domain.CorrectionTaskEntity;
+import org.keda.errorfreetext.core.domain.TaskStatus;
+import org.keda.errorfreetext.core.repositories.CorrectionTaskRepository;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class CorrectionTaskProviderTest {
+
+    @Mock
+    private CorrectionTaskRepository repository;
+
+    private CorrectionTaskProvider provider;
+
+    private static final int PAGE_SIZE = 10;
+
+    @BeforeEach
+    void setUp() {
+        provider = new CorrectionTaskProvider(repository, PAGE_SIZE);
+    }
+
+    @Test
+    void shouldChangeStatusOfReturnedEntitiesToProcessing() {
+        CorrectionTaskEntity task1 = newTask();
+        CorrectionTaskEntity task2 = newTask();
+        List<CorrectionTaskEntity> tasks = List.of(task1, task2);
+        when(repository.findByTaskStatusOrderByCreatedAtAsc(
+                TaskStatus.NEW, Pageable.ofSize(PAGE_SIZE))
+        ).thenReturn(tasks);
+        List<CorrectionTaskEntity> result = provider.getNewTasks();
+
+        assertNotNull(result);
+        assertEquals(tasks.size(), result.size());
+        for(CorrectionTaskEntity task: result) {
+            assertEquals(TaskStatus.PROCESSING, task.getTaskStatus());
+        }
+        assertEquals(TaskStatus.PROCESSING, result.get(0).getTaskStatus());
+        assertEquals(TaskStatus.PROCESSING, result.get(1).getTaskStatus());
+    }
+
+    private CorrectionTaskEntity newTask() {
+        CorrectionTaskEntity task = new CorrectionTaskEntity();
+        task.setTaskStatus(TaskStatus.NEW);
+        return task;
+    }
+
+}
---
+++ error-free-text-app/src/test/java/org/keda/errorfreetext/core/services/ProcessCorrectionTasksServiceImplTest.java
@@ -0,0 +1,67 @@
+package org.keda.errorfreetext.core.services;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.keda.errorfreetext.core.domain.CorrectionTaskEntity;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class ProcessCorrectionTasksServiceImplTest {
+
+    @Mock
+    private CorrectionTaskProvider taskProvider;
+
+    @Mock
+    private SingleCorrectionTaskProcessor taskProcessor;
+
+    @InjectMocks
+    private ProcessCorrectionTasksServiceImpl service;
+
+    @Test
+    void shouldProcessSameNumberOfTasksAsProvided() {
+        List<CorrectionTaskEntity> tasks = List.of(
+                newTask(),
+                newTask(),
+                newTask()
+        );
+
+        when(taskProvider.getNewTasks())
+                .thenReturn(tasks);
+        when(taskProcessor.process(any()))
+                .thenAnswer(invocation -> invocation.getArgument(0));
+
+        service.processNewTasks();
+
+        verify(taskProvider, times(1)).getNewTasks();
+        verify(taskProcessor, times(tasks.size())).process(any(CorrectionTaskEntity.class));
+    }
+
+    @Test
+    void shouldNotProcessAnythingWhenNoNewTasksFound() {
+        List<CorrectionTaskEntity> tasks = List.of();
+
+        when(taskProvider.getNewTasks())
+                .thenReturn(tasks);
+
+        service.processNewTasks();
+
+        verify(taskProvider, times(1)).getNewTasks();
+        verifyNoInteractions(taskProcessor);
+    }
+
+
+    private CorrectionTaskEntity newTask() {
+        return new CorrectionTaskEntity();
+    }
+
+}
