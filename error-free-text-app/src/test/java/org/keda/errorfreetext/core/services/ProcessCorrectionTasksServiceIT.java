471ab1bfc5daa50bc131cbf9ea3df27923194d08
Alexander Keda <alex.keda94@yandex.ru>
2026-02-15 18:20:16 +0300

    add integration tests for ProcessCorrectionTasksService

--- error-free-text-app/src/test/java/org/keda/errorfreetext/core/services/ProcessCorrectionTasksServiceIT.java
+++ error-free-text-app/src/test/java/org/keda/errorfreetext/core/services/ProcessCorrectionTasksServiceIT.java
@@ -1,20 +1,29 @@
 package org.keda.errorfreetext.core.services;
 
 import com.github.tomakehurst.wiremock.WireMockServer;
-import org.junit.jupiter.api.Disabled;
+import com.github.tomakehurst.wiremock.client.WireMock;
 import org.junit.jupiter.api.Test;
+import org.keda.errorfreetext.core.domain.CorrectionTaskEntity;
+import org.keda.errorfreetext.core.domain.Language;
 import org.keda.errorfreetext.core.domain.TaskStatus;
 import org.keda.errorfreetext.core.repositories.CorrectionTaskRepository;
+import org.keda.errorfreetext.properties.YandexSpellerProperties;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@Disabled
 @SpringBootTest
 @Transactional
 @AutoConfigureWireMock(port = 8085)
@@ -29,12 +38,177 @@ class ProcessCorrectionTasksServiceIT {
     @Autowired
     private CorrectionTaskRepository repository;
 
+    @Autowired
+    private YandexSpellerProperties yandexSpellerProperties;
+
     @Test
-    void shouldNotCrash() {
+    void shouldProcessCorrectionTaskSuccessfullyWhenTextIsCorrect() {
+        String originalText = "text";
+        var newTask = CorrectionTaskEntity.builder()
+                .taskUuid(UUID.randomUUID())
+                .originalText(originalText)
+                .language(Language.EN)
+                .taskStatus(TaskStatus.NEW)
+                .build();
+        repository.save(newTask);
+
+        wireMockServer.stubFor(WireMock.post("/checkTexts")
+                .willReturn(WireMock.aResponse()
+                        .withStatus(200)
+                        .withHeader("Content-Type", "application/json")
+                        .withBody("[[]]")));
+
         tasksService.processNewTasks();
-        var taskOpt = repository.findByTaskUuid(UUID.fromString("96e46569-5729-40d1-a707-3628b1ca53b1"));
+
+
+        var taskOpt = repository.findByTaskUuid(newTask.getTaskUuid());
         assertTrue(taskOpt.isPresent());
-        assertEquals(TaskStatus.ERROR, taskOpt.get().getTaskStatus());
+        var doneTask = taskOpt.get();
+        assertEquals(TaskStatus.DONE, doneTask.getTaskStatus());
+        assertEquals(originalText, doneTask.getCorrectedText());
+    }
+
+    @Test
+    void shouldProcessCorrectionTaskSuccessfullyWhenTextHasError() {
+        String originalText = "Helo!";
+        String correctedText = "Hello!";
+        var newTask = CorrectionTaskEntity.builder()
+                .taskUuid(UUID.randomUUID())
+                .originalText(originalText)
+                .language(Language.EN)
+                .taskStatus(TaskStatus.NEW)
+                .build();
+        repository.save(newTask);
+
+        wireMockServer.stubFor(WireMock.post("/checkTexts")
+                .willReturn(WireMock.aResponse()
+                        .withStatus(200)
+                        .withHeader("Content-Type", "application/json")
+                        .withBody("[[{\"code\": 1,\"pos\": 0,\"row\": 0,\"col\": 0,\"len\": 4,\"word\": \"Helo\",\"s\": [\"Hello\",\"Halo\",\"Help\"]}]]")));
+
+        tasksService.processNewTasks();
+
+
+        var taskOpt = repository.findByTaskUuid(newTask.getTaskUuid());
+        assertTrue(taskOpt.isPresent());
+        var doneTask = taskOpt.get();
+        assertEquals(TaskStatus.DONE, doneTask.getTaskStatus());
+        assertEquals(correctedText, doneTask.getCorrectedText());
+    }
+
+    @Test
+    void shouldProcessCorrectionTaskWithErrorWhenApiResponseIsHttp500() {
+        String originalText = "text";
+        var newTask = CorrectionTaskEntity.builder()
+                .taskUuid(UUID.randomUUID())
+                .originalText(originalText)
+                .language(Language.EN)
+                .taskStatus(TaskStatus.NEW)
+                .build();
+        repository.save(newTask);
+
+        wireMockServer.stubFor(WireMock.post("/checkTexts")
+                .willReturn(WireMock.aResponse()
+                        .withStatus(500)
+                        .withHeader("Content-Type", "application/json")
+                        .withBody("")));
+
+        tasksService.processNewTasks();
+
+        var taskOpt = repository.findByTaskUuid(newTask.getTaskUuid());
+        assertTrue(taskOpt.isPresent());
+        var doneTask = taskOpt.get();
+        assertEquals(TaskStatus.ERROR, doneTask.getTaskStatus());
+        assertNull(doneTask.getCorrectedText());
+        assertFalse(doneTask.getErrorMessage().isBlank());
+    }
+
+    @Test
+    void shouldRetryWhenApiRespondsHttp500() {
+        int maxRetryAttempts = yandexSpellerProperties.retryMaxAttempts();
+        String originalText = "text";
+        var newTask = CorrectionTaskEntity.builder()
+                .taskUuid(UUID.randomUUID())
+                .originalText(originalText)
+                .language(Language.EN)
+                .taskStatus(TaskStatus.NEW)
+                .build();
+        repository.save(newTask);
+
+        wireMockServer.stubFor(WireMock.post("/checkTexts")
+                .willReturn(WireMock.aResponse()
+                        .withStatus(500)
+                        .withHeader("Content-Type", "application/json")
+                        .withBody("")));
+
+        tasksService.processNewTasks();
+
+       wireMockServer.verify(maxRetryAttempts,WireMock.postRequestedFor(WireMock.urlEqualTo("/checkTexts")));
+    }
+
+    @Test
+    void shouldProcessCorrectionTaskWithErrorWhenApiResponseTimesOut() {
+        int readTimeout = yandexSpellerProperties.readTimeout();
+        String originalText = "text";
+        var newTask = CorrectionTaskEntity.builder()
+                .taskUuid(UUID.randomUUID())
+                .originalText(originalText)
+                .language(Language.EN)
+                .taskStatus(TaskStatus.NEW)
+                .build();
+        repository.save(newTask);
+
+        wireMockServer.stubFor(WireMock.post("/checkTexts")
+                .willReturn(WireMock.aResponse()
+                        .withFixedDelay(readTimeout * 2)
+                        .withStatus(200)
+                        .withHeader("Content-Type", "application/json")
+                        .withBody("[[]]")));
+
+        tasksService.processNewTasks();
+
+        var taskOpt = repository.findByTaskUuid(newTask.getTaskUuid());
+        assertTrue(taskOpt.isPresent());
+        var doneTask = taskOpt.get();
+        assertEquals(TaskStatus.ERROR, doneTask.getTaskStatus());
+        assertNull(doneTask.getCorrectedText());
+        assertFalse(doneTask.getErrorMessage().isBlank());
+    }
+
+    @Test
+    void shouldProcessLongCorrectTextSuccessfully() {
+        String originalText = readFromFile("/test-data/large-text.txt");
+        var newTask = CorrectionTaskEntity.builder()
+                .taskUuid(UUID.randomUUID())
+                .originalText(originalText)
+                .language(Language.RU)
+                .taskStatus(TaskStatus.NEW)
+                .build();
+        repository.save(newTask);
+
+        wireMockServer.stubFor(WireMock.post("/checkTexts")
+                .willReturn(WireMock.aResponse()
+                        .withStatus(200)
+                        .withHeader("Content-Type", "application/json")
+                        .withBody("[[],[]]")));
+
+        tasksService.processNewTasks();
+
+
+        var taskOpt = repository.findByTaskUuid(newTask.getTaskUuid());
+        assertTrue(taskOpt.isPresent());
+        var doneTask = taskOpt.get();
+        assertEquals(TaskStatus.DONE, doneTask.getTaskStatus());
+        assertEquals(originalText, doneTask.getCorrectedText());
+    }
+
+    private String readFromFile(String filePath) {
+        try {
+            Path path = new ClassPathResource(filePath).getFile().toPath();
+            return Files.readString(path);
+        } catch (IOException e) {
+            throw new RuntimeException("Error reading file from classpath: " + filePath, e);
+        }
     }
 
 }
