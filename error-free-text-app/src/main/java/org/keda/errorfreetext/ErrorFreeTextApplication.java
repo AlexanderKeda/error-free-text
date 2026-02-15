76331d3821ab54773635ace1410a33b03b110433
Alexander Keda <alex.keda94@yandex.ru>
2026-02-15 15:45:38 +0300

    refactor app configuration

--- error-free-text-app/src/main/java/org/keda/errorfreetext/ErrorFreeTextApplication.java
+++ error-free-text-app/src/main/java/org/keda/errorfreetext/ErrorFreeTextApplication.java
@@ -3,13 +3,11 @@ package org.keda.errorfreetext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableTransactionManagement
 @ConfigurationPropertiesScan
-@EnableAsync
 public class ErrorFreeTextApplication {
     public static void main(String[] args) {
         SpringApplication.run(ErrorFreeTextApplication.class, args);
